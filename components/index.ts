dc37cead4dbce77eae1124f4c36c8b9f3a4c1121
Michael Owuor <michael.owuor@gmail.com>
2024-02-07 05:09:37 +0300

    feat: add button component to index.ts and update dependencies

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+/* component blocks */
+
+/* notification components */
+
+/* shadcn ui components */
+import { Button } from "@/components/ui/button";
+
+/* component widgets */
+
+/* export components */
+export { Button };
